b75a2354ae00f61e7788f9cbb8799e8b66bbb40b
Jailsonwin <jailson.ferens@aluno.unc.br>
2022-07-13 10:37:18 -0300

    Inserting content into the home

--- src/components/Navbar/Clock/index.tsx
+++ src/components/Navbar/Clock/index.tsx
@@ -1,13 +1,28 @@
 import styles from './Clock.module.scss';
+import { useEffect, useState } from 'react';
 
 export default function Clock() {
+
+    const [date, setDate] = useState(new Date());
+
+    function ClockUpdate(){
+        setDate(new Date());
+    }
+
+    useEffect(()=>{
+        const timer = setInterval(ClockUpdate, 1000);
+        return function cleanup(){
+            clearInterval(timer);
+        }
+    },[]);
+
     return(
         <div className={styles.container}>
             <h2>
-                00:00
+                {date.toLocaleTimeString([], {hour: "2-digit", minute:"2-digit"})}
             </h2>
             <p>
-                Dia-por-Extenso, 00 de mes de Ano
+                {date.toLocaleDateString([], { dateStyle:"full"})}
             </p>
         </div>
     );
