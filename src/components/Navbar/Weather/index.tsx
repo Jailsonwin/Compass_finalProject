cf622f5bccd86d5c1e27877b98482ab149a00adf
Jailsonwin <jailson.ferens@aluno.unc.br>
2022-07-14 13:39:14 -0300

    Adding API Wheather

--- src/components/Navbar/Weather/index.tsx
+++ src/components/Navbar/Weather/index.tsx
@@ -1,16 +1,34 @@
 import styles from './Weather.module.scss';
 import cloud from 'assets/cloud.svg';
+import { useState } from 'react';
 
-export default function Weather() {
+export default function Weather() { 
+    const [local, setCity] = useState<string>();
+    const [temp, setTemp] = useState<number>();
+    
+    function fetchWeather() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let lat = position.coords.latitude;
+            let long = position.coords.longitude;        
+            fetch(`https://api.openweathermap.org/data/2.5/weather/?lat=${lat}&lon=${long}&units=metric&APPID=bf0de12f9df4e3c21f2fb18a7606c041`)
+            .then(res => res.json())
+            .then(result => {
+                const { main, name } = result;
+                setCity(`${name} - SC`)
+                setTemp(Math.round(main.temp));
+            });
+        });
+    }
+
     return(
-        <div className={styles.container}>
+        <div className={styles.container} onLoad={()=> fetchWeather()} >
             <h3>
-                Papanduva - SC
+                {local}
             </h3>
             <div className={styles.temp}>
                 <img src={cloud} alt="Cloud" />
                 <h2>
-                    19º
+                    {temp}º
                 </h2>
             </div>
         </div>
